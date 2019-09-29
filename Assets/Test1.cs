fb9346fe6ee8fda9c345bcd71feb797926dd1079
Ofekod Unity <1xofekx1@gmail.com>
2019-09-30 02:09:48 +0300

    added stuff with .gitignore

---
+++ Assets/Test1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * 1000);
+            }
+        }
+    }
+}
